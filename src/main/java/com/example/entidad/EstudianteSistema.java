6d0f6046dfe5919d2e6411d3be92941782d649c9
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-05-28 22:32:14 -0500

    Agregando atributos a la entidad EstudianteSistema

--- src/main/java/com/example/entidad/EstudianteSistema.java
+++ src/main/java/com/example/entidad/EstudianteSistema.java
@@ -7,6 +7,16 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EstudianteSistema {
+
+    private Integer codigo_estudiante;
+
+    private String correo;
+
+    private String contrasena;
+
+    private Estudiante_Universidad estudiante_Universidad;
 
 }
