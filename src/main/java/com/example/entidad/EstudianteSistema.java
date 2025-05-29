7916086a68af20d3ce45f816bec2958d76714a6c
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-05-28 22:34:31 -0500

    Validador en la entidad EstudianteSistema

--- src/main/java/com/example/entidad/EstudianteSistema.java
+++ src/main/java/com/example/entidad/EstudianteSistema.java
@@ -1,6 +1,12 @@
 package com.example.entidad;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class EstudianteSistema {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo_estudiante;
 
+    @NotNull
+    @NotBlank(message="Ingrese un correo")
     private String correo;
 
-    private String contrasena;
+    @NotNull
+    @NotBlank(message="Ingrese una contraseña")
+    private String password;
+
 
     private Estudiante_Universidad estudiante_Universidad;
 
