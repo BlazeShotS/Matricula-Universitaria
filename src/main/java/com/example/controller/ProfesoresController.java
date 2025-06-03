71de6067525ca89d72cbdc71aa79e3ebe330afa8
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-06-03 01:33:42 -0500

    Services y Controller listo de la entidad Profesores

--- src/main/java/com/example/controller/ProfesoresController.java
+++ src/main/java/com/example/controller/ProfesoresController.java
@@ -1,14 +1,53 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entidad.Carrera;
+import com.example.entidad.Profesores;
+import com.example.services.ProfesoresService;
+
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 
 @Entity
 @RequestMapping(value = "api/profesores", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class ProfesoresController {
 
+    private final ProfesoresService profesoresService;
     
+    @GetMapping("listar")
+    public List <Profesores> listarProfesores() {
+        return profesoresService.listar();
+    }
+
+    @PostMapping("insertar")
+    public Profesores insertarProfesor(@RequestBody Profesores profesores) {                
+        return profesoresService.insertar(profesores);
+    }
+
+    @PutMapping("actualizar/{id}")
+    public Profesores actualizarProfesor(@PathVariable Integer id, @RequestBody Profesores profesores) {
+        profesores.setId_profesor(id);
+        return profesoresService.actualizar(id, profesores);
+    }
+
+    @DeleteMapping("eliminar/{id}")
+    public ResponseEntity <String> eliminarProfesor(@PathVariable Integer id){
+        profesoresService.eliminar(id);
+        return ResponseEntity.ok("Carrera eliminado correctamente con Id: "+id);
+    }
+
+
 
 }
